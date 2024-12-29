82e74927c0edff0ffd4346c05975eab9cd308f48
Avelgar <73475957+Avelgar@users.noreply.github.com>
2024-12-29 05:27:28 +0300

    Cделал адапативный хедер и смену темы
    ебал в рот коммиты

---
+++ public/scripts/theme.js
@@ -0,0 +1,20 @@
+const themeToggles = document.querySelectorAll('.themeToggle');
+
+function setTheme(theme) {
+    document.body.classList.toggle('dark-theme', theme === 'dark');
+}
+
+function toggleTheme() {
+    const currentTheme = localStorage.getItem('theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    localStorage.setItem('theme', newTheme);
+    setTheme(newTheme);
+}
+
+themeToggles.forEach(button => {
+    button.addEventListener('click', toggleTheme);
+});
+
+const savedTheme = localStorage.getItem('theme') || 'light';
+setTheme(savedTheme);
+
